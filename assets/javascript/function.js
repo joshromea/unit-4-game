a0bec6906ad9a5807ba5e56eb3f1b96b1037f14f
Josh Romea <josh.romea@gmail.com>
2019-03-26 11:21:01 -0700

    commit

---
+++ assets/javascript/function.js
@@ -0,0 +1,19 @@
+$( "document").ready(function(){
+    var friezaHitpoints=Math.floor(Math.random()*200-20)
+
+$("#target-hp").html(friezaHitpoints);
+
+var trunks = math.floor(math.random()* 50+19);
+var vegeta = math.floor(math.random()* 50+19);
+var goku = math.floor(math.random()* 50+19);
+var gohan = math.floor(math.random()* 50+19);
+
+var wins = 0;
+var losses = 0;
+
+
+
+
+
+
+});
